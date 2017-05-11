85903c58a69e35b6b40459da7f72fdcbc83165e3
Roy de Kleijn <roy.de.kleijn@powerhouse.nl>
2017-05-11 09:13:26 +0300

    updated source

--- src/test/java/answers/AbstractTestBase.java
+++ src/test/java/answers/AbstractTestBase.java
@@ -1,7 +1,10 @@
 package answers;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -9,21 +12,21 @@ import util.File;
 import util.Os;
 
 public abstract class AbstractTestBase {
-	protected WebDriver driver;
-	// protected RemoteWebDriver driver;
+	// protected WebDriver driver;
+	protected RemoteWebDriver driver;
 
 	@BeforeMethod(alwaysRun = true)
-	public void setUp() {
+	public void setUp() throws MalformedURLException {
 		// Determine OS
 		final String os = Os.getOs();
 
 		// Create a new instance of the Firefox driver
 		System.setProperty("webdriver.chrome.driver", File.findFileOnPath("drivers/" + os + "/chromedriver")); // geckodriver
 		// System.setProperty("webdriver.gecko.driver", findFileOnPath("drivers/mac/geckodriver")); // geckodriver
-		driver = new ChromeDriver();
+		// driver = new ChromeDriver();
 
-		// final DesiredCapabilities dr = DesiredCapabilities.chrome();
-		// driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), dr);
+		final DesiredCapabilities dr = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("http://192.168.99.100:4444/wd/hub"), dr);
 
 	}
 
